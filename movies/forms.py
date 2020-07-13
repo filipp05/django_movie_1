3345d02bd904738f697266a516b39fff5bca6466
Philip NIkulenko <59737526+filipp05@users.noreply.github.com>
2020-07-13 19:25:14 +0300

    Initial commit

---
+++ movies/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from snowpenguin.django.recaptcha3.fields import ReCaptchaField
+
+from .models import Reviews, Rating, RatingStar
+
+
+class ReviewForm(forms.ModelForm):
+    """Форма отзывов"""
+    captcha = ReCaptchaField()
+
+    class Meta:
+        model = Reviews
+        fields = ("name", "email", "text", "captcha")
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-controll border"}),
+            "email": forms.EmailInput(attrs={"class": "form-controll border"}),
+            "text": forms.Textarea(attrs={"class": "form-controll border"})
+        }
+
+
+class RatingForm(forms.ModelForm):
+    """Форма добавления рейтинга"""
+    star = forms.ModelChoiceField(queryset=RatingStar.objects.all(),
+                                  widget=forms.RadioSelect(), empty_label=None)
+
+    class Meta:
+        model = Rating
+        fields = ('star',)
